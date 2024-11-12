06f0094befbfaa83013392e4549875d5afe772ed
BorisBritva572 <146216498+BorisBritva572@users.noreply.github.com>
2024-11-13 00:48:39 +0300

    Добавил переход из главного меню на 1 уровень

--- WAVE-TIME-MASS/Assets/MainMenu.cs
+++ WAVE-TIME-MASS/Assets/MainMenu.cs
@@ -1,24 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-
-    //}
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-
-    //}
-
     public void NewGame()
     {
         Debug.Log("новая игра");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void Exit()
     {
