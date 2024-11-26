8ed361658848a001ff3db471dc68234231f691c7
Денис Рыпунов <rypunov2005@bk.ru>
2024-11-26 22:38:11 +0300

    Изменены кнопки для артефактов и исправлена логика финиша

--- WAVE-TIME-MASS/Assets/Scripts/Finish.cs
+++ WAVE-TIME-MASS/Assets/Scripts/Finish.cs
@@ -5,10 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
+    public GameObject textTheEnd;
+    private Hero playerController;
+
+    void Start()
+    {
+        // Ищем игрока
+        playerController = FindObjectOfType<Hero>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if ((collision.tag == "Player") && (SceneManager.GetActiveScene().buildIndex < 2))
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        if (SceneManager.GetActiveScene().buildIndex == 2)
+        {
+            textTheEnd.SetActive(true); // Отображаем финальный экран
+            playerController.canControl = false; // Блокируем управление
+        }
 
         Destroy(gameObject);
     }
